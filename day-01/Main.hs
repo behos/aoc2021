dbc744f01c7841d800ffb188d496d659cdff5576
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2021-12-01 09:25:06 +0200

    Shring the solution since we use the same input in both parts

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -5,23 +5,17 @@ import           Lib
 main :: IO ()
 main = do
   content <- readFile "inputs/day-01.txt"
+  let input = map Lib.toInt $ lines content
   putStrLn "Part 1"
-  putStrLn $ show $ part1 content
+  putStrLn $ show $ countIncreasing input
   putStrLn "Part 2"
-  putStrLn $ show $ part2 content
-
-
-part1 :: String -> Int
-part1 input = countIncreasing $ map Lib.toInt $ lines input
+  putStrLn $ show $ countWindows input
 
 countIncreasing :: [Int] -> Int
 countIncreasing (a:rest@(b:_))
   | a < b = 1 + countIncreasing rest
   | otherwise = countIncreasing rest
 countIncreasing _ = 0
-
-part2 :: String -> Int
-part2 input = countWindows $ map Lib.toInt $ lines input
 
 countWindows :: [Int] -> Int
 countWindows (a:rest@(b:c:d:_))
