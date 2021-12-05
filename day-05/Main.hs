ad66c75c1ba16e4c967be76ddcaafa562791d250
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2021-12-05 13:18:02 +0200

    Use composition instead of apply

--- day-05/Main.hs
+++ day-05/Main.hs
@@ -17,8 +17,7 @@ type Point = (Int, Int)
 type Line = (Point, Point)
 
 parseInput :: String -> [Line]
-parseInput content = do
-  toLine <$> lines content
+parseInput = (<$>) toLine . lines
 
 toLine :: String -> Line
 toLine raw = do
@@ -37,10 +36,7 @@ expand :: Line -> [Point]
 expand line@(p1, p2)
   | p1 == p2 = [p1]
   | otherwise = p1 : expand
-    ( addPoints p1
-      $ direction line
-    , p2
-    )
+    ((addPoints p1 . direction) line, p2)
 
 direction :: Line -> Point
 direction ((x1, y1), (x2, y2)) = pointUnit (x2 - x1, y2 - y1)
@@ -50,20 +46,19 @@ pointUnit (x, y) = (intUnit x, intUnit y)
 
 intUnit :: Int -> Int
 intUnit 0 = 0
-intUnit x = div x $ abs x
+intUnit x = (div x . abs) x
 
 addPoints :: Point -> Point -> Point
 addPoints (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 findVents :: [Line] -> Int
-findVents lines = length
-  $ filter (>= 2)
-  $ HashMap.elems
-  $ countPoints
-  $ concat
-  $ expand <$> lines
+findVents = length
+  . filter (>= 2)
+  . HashMap.elems
+  . countPoints
+  . concat
+  . (expand <$>)
 
 countPoints :: [Point] -> HashMap.HashMap Point Int
 countPoints []     = HashMap.empty
-countPoints (p:ps) = HashMap.insertWith (+) p 1
-  $ countPoints ps
+countPoints (p:ps) = (HashMap.insertWith (+) p 1 . countPoints) ps
