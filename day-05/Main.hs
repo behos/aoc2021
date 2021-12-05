9b0e18d986719c7a104502d15f3a5590d926397c
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2021-12-05 13:33:14 +0200

    Remove unnecessary `do`s

--- day-05/Main.hs
+++ day-05/Main.hs
@@ -20,14 +20,14 @@ parseInput :: String -> [Line]
 parseInput = (<$>) toLine . lines
 
 toLine :: String -> Line
-toLine raw = do
+toLine raw =
   let (from:to:_) = splitOn " -> " raw
-  (toPoint from, toPoint to)
+  in (toPoint from, toPoint to)
 
 toPoint :: String -> Point
-toPoint raw = do
+toPoint raw =
   let (x:y:_) = splitOn "," raw
-  (Lib.toInt x, Lib.toInt y)
+  in (Lib.toInt x, Lib.toInt y)
 
 isStraight :: Line -> Bool
 isStraight ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
@@ -35,8 +35,10 @@ isStraight ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
 expand :: Line -> [Point]
 expand line@(p1, p2)
   | p1 == p2 = [p1]
-  | otherwise = p1 : expand
-    ((addPoints p1 . direction) line, p2)
+  | otherwise = p1 : expand (
+    addPoints p1 $ direction line
+    , p2
+    )
 
 direction :: Line -> Point
 direction ((x1, y1), (x2, y2)) = pointUnit (x2 - x1, y2 - y1)
@@ -46,7 +48,7 @@ pointUnit (x, y) = (intUnit x, intUnit y)
 
 intUnit :: Int -> Int
 intUnit 0 = 0
-intUnit x = (div x . abs) x
+intUnit x = div x $ abs x
 
 addPoints :: Point -> Point -> Point
 addPoints (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
@@ -61,4 +63,4 @@ findVents = length
 
 countPoints :: [Point] -> HashMap.HashMap Point Int
 countPoints []     = HashMap.empty
-countPoints (p:ps) = (HashMap.insertWith (+) p 1 . countPoints) ps
+countPoints (p:ps) = HashMap.insertWith (+) p 1 $ countPoints ps
