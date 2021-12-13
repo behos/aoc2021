4be3f2a8645ae83b8874f44863c61a759d2a55e3
Giorgos Georgiou <giorgos.georgiou@outlook.com>
2021-12-13 09:11:55 +0200

    Use foldl' now that I know about it

--- day-10/Main.hs
+++ day-10/Main.hs
@@ -10,36 +10,30 @@ main = do
   content <- readFile "inputs/day-10.txt"
   let errors = parse [] <$> lines content
   putStrLn "Part 1"
-  print $ scoreCorrupted errors
+  print $ sum $ scoreCorrupted <$> errors
   putStrLn "Part 2"
   print $ getCompletionScore errors
 
 parse :: [Char] -> String -> SyntaxError
-parse s []            = Incomplete s
+parse s [] = Incomplete s
 parse (o:os) (c:p)
   | matching o == c = parse os p
 parse s (c:p)
   | opening c = parse (c:s) p
   | otherwise = Corrupted c
 
-scoreCorrupted :: [SyntaxError] -> Int
-scoreCorrupted []               = 0
-scoreCorrupted (Incomplete _:r) = scoreCorrupted r
-scoreCorrupted (Corrupted c:r)  = points c + scoreCorrupted r
+scoreCorrupted :: SyntaxError -> Int
+scoreCorrupted (Incomplete _) = 0
+scoreCorrupted (Corrupted c)  = points c
 
 getCompletionScore :: [SyntaxError] -> Int
 getCompletionScore err = let
-  scores = scoreCompletions err
+  scores = filter (>0) $ scoreCompletion <$> err
   in sort scores !! div (length scores) 2
 
-scoreCompletions :: [SyntaxError] -> [Int]
-scoreCompletions []               = []
-scoreCompletions (Incomplete s:r) = scoreString (reverse s):scoreCompletions r
-scoreCompletions (_:r)            = scoreCompletions r
-
-scoreString :: [Char] -> Int
-scoreString []    = 0
-scoreString (c:r) = points c + 5 * scoreString r
+scoreCompletion :: SyntaxError -> Int
+scoreCompletion (Incomplete s) = foldl' (\p c -> points c + 5 * p) 0 s
+scoreCompletion _              = 0
 
 opening :: Char -> Bool
 opening c = c == '(' || c == '[' || c == '{' || c == '<'
